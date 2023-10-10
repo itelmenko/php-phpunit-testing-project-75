e8a5fcf9a4df2f469071bd39ca901c579d5aa88b
Igor Telmenko <igor-telmenko@yandex.ru>
2023-10-10 21:22:36 +0300

    Добавление конфигурационных файлов вспомогательных утилит

---
+++ tests/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Geekk\PayselectionPaymentsPhp\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
