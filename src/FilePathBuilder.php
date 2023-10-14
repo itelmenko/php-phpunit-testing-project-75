088612b05058b881e7b8b1b6323468ec901219c6
Igor Telmenko <igor-telmenko@yandex.ru>
2023-10-14 10:38:26 +0300

    Добавлена возможность указать расширение файла

--- src/FilePathBuilder.php
+++ src/FilePathBuilder.php
@@ -5,13 +5,13 @@ namespace Hexlet\Code;
 class FilePathBuilder
 {
 
-    public function buildFilePath(string $url): string
+    public function buildFilePath(string $url, ?string $extension = null): string
     {
         $parts = parse_url($url);
         $url = $parts['host'].$parts['path'];
         $step1 = preg_replace('/[^0-9A-z]/', '-', $url);
         $step2 = preg_replace('/-+/', '-', $step1);
 
-        return trim($step2, '-');
+        return trim($step2, '-').(empty($extension) ? '' : ".$extension");
     }
 }
--- tests/FilePathBuilderTest.php
+++ tests/FilePathBuilderTest.php
@@ -8,20 +8,31 @@ use PHPUnit\Framework\TestCase;
 class FilePathBuilderTest extends TestCase
 {
 
+    /**
+     * @return array<string,string[]>
+     */
     public function urlsDataProvider(): array
     {
         return [
             'simple_url' => [
                 'https://some.domain.net/with/path/',
+                null,
                 'some-domain-net-with-path'
             ],
             'url_with_file_name' => [
                 'https://some.domain.net/with/path/and-dot.html',
+                null,
                 'some-domain-net-with-path-and-dot-html'
             ],
             'url_with_query' => [
                 'https://some.domain.net/with/path/?param1=foo&param2=bar',
+                null,
                 'some-domain-net-with-path'
+            ],
+            'file_with_extension' => [
+                'https://some.domain.net/with/path/',
+                'html',
+                'some-domain-net-with-path.html'
             ]
         ];
     }
@@ -29,10 +40,10 @@ class FilePathBuilderTest extends TestCase
     /**
      * @dataProvider urlsDataProvider
      */
-    public function testItConvertSpecialSymbolsToMinus($url, $expected): void
+    public function testItConvertSpecialSymbolsToMinus(string $url, ?string $extension, string $expected): void
     {
         $pathBuilder = new FilePathBuilder();
-        $actual = $pathBuilder->buildFilePath($url);
+        $actual = $pathBuilder->buildFilePath($url, $extension);
         $this->assertEquals($expected, $actual);
     }
 }
