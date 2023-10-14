69adc3743be4f42814a9a34019f0d05b5556500b
Igor Telmenko <igor-telmenko@yandex.ru>
2023-10-14 19:50:01 +0300

    В тесте консольной команды используем vfs

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -10,12 +10,25 @@ use Hexlet\Code\Command;
 use Hexlet\Code\FilePathBuilder;
 use Hexlet\Code\Loader;
 use Hexlet\Code\Storage;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class CommandTest extends TestCase
 {
+
+    public function setUp(): void
+    {
+        vfsStream::setup('base');
+    }
+
+    private function getVirtualPath(?string $path = null): string
+    {
+        $directoryPath = vfsStream::url('base');
+
+        return empty($path) ? $directoryPath : "{$directoryPath}/".ltrim($path, '/');
+    }
 
     public function testItLoadsContent(): void
     {
@@ -28,6 +41,9 @@ class CommandTest extends TestCase
         $handlerStack = HandlerStack::create($mock);
         $client = new Client(['handler' => $handlerStack]);
 
+        $targetPathUrl = $this->getVirtualPath('loader');
+        mkdir($targetPathUrl);
+
         $loader = new Loader($client);
         $application = new Application();
         $command = new Command('page-loader', $loader, new FilePathBuilder(), new Storage());
@@ -35,7 +51,7 @@ class CommandTest extends TestCase
 
         $command = $application->find('page-loader');
         $testerCommand = new CommandTester($command);
-        $testerCommand->execute(['url' => $url, '--output' => '/tmp']);
+        $testerCommand->execute(['url' => $url, '--output' => $targetPathUrl]);
 
         $result = @file_get_contents('/tmp/some-domain-net-page-path.html');
         $this->assertEquals($content, $result);
